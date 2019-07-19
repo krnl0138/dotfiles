d70d2f3fe0c9c21cdc279f0c24fb1c648de2bde9
kudinov <krnl03182@gmail.com>
2019-07-19 06:00:01 +0300

    regular auto update

--- home/scripts/scripts/bg.sh
+++ home/scripts/scripts/bg.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-swaybg VGA-1 `find /home/gppk/pics/wall | shuf -n 1` fill &
+swaybg VGA-1 "$(find /home/gppk/pics/wall | shuf -n 1)" fill &
 OLD_BG=$!
 while true; do
-    swaybg VGA-1 `find /home/gppk/pics/wall | shuf -n 1` fill &
+    swaybg VGA-1 "$(find /home/gppk/pics/wall | shuf -n 1)" fill &
     NEXT_BG=$!
     sleep 30
     kill $OLD_BG
