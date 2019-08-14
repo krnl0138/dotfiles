14219d61f2e4a818801ac0b09373ffccb58af14a
kudinov <krnl03182@gmail.com>
2019-08-14 20:00:01 +0300

    regular auto update

--- home/scripts/scripts/bg.sh
+++ home/scripts/scripts/bg.sh
@@ -1,10 +1,6 @@
 #!/bin/sh
-swaybg VGA-1 "$(find /home/gppk/pics/wall | shuf -n 1)" fill &
-OLD_BG=$!
 while true; do
-    swaybg VGA-1 "$(find /home/gppk/pics/wall | shuf -n 1)" fill &
-    NEXT_BG=$!
-    sleep 30
-    kill $OLD_BG
-    OLD_BG=$NEXT_BG
+    img=$(find "$HOME"/pics/wall/* | shuf -n 1)
+    swaymsg output "*" bg $img fill
+    sleep 600
 done
