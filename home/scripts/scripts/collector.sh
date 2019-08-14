d083abe343f770fc0a6da836d4bd8df49a945a0d
kudinov <krnl03182@gmail.com>
2019-08-14 22:25:01 +0300

    regular auto update

--- home/scripts/scripts/collector.sh
+++ home/scripts/scripts/collector.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 location="/home/gppk/Downloads"
-style=" -maxdepth 1 -type f"
-find "$location$style" \( -iname \*pdf \) -exec mv {} ~/Downloads/pdfs  \;  && \
-find "$location$style" \( -iname \*djvu \) -exec mv {} ~/Downloads/djvus  \; && \
-find "$location$style" \( -iname \*docx \) -exec mv {} ~/Downloads/docxs \; && \
-find "$location$style" \( -iname \*epub \) -exec mv {} ~/Downloads/epubs \; && \
-find "$location$style" \( -iname \*fb2 \) -exec mv {} ~/Downloads/fb2s  \; && \
-find "$location$style" \( -iname \*ppt \) -exec mv {} ~/Downloads/ppts  \; && \
-find "$location$style" \( -iname \*zip \) -exec mv {} ~/Downloads/zips  \; && \
-find "$location$style" \( -iname \*jpg -o -iname \*jpeg -o -iname \*jpg_ -o \
+style="-maxdepth 1 -type f"
+find "$style" "$location" \( -iname \*pdf \) -exec mv {} ~/Downloads/pdfs  \;  && \
+find "$style" "$location" \( -iname \*djvu \) -exec mv {} ~/Downloads/djvus  \; && \
+find "$style" "$location" \( -iname \*docx \) -exec mv {} ~/Downloads/docxs \; && \
+find "$style" "$location" \( -iname \*epub \) -exec mv {} ~/Downloads/epubs \; && \
+find "$style" "$location" \( -iname \*fb2 \) -exec mv {} ~/Downloads/fb2s  \; && \
+find "$style" "$location" \( -iname \*ppt \) -exec mv {} ~/Downloads/ppts  \; && \
+find "$style" "$location" \( -iname \*zip \) -exec mv {} ~/Downloads/zips  \; && \
+find "$style" "$location" \( -iname \*jpg -o -iname \*jpeg -o -iname \*jpg_ -o \
                                         -iname \*jpe -o -iname \*png -o -iname \*png_ -o \
                                         -iname \*gif \) -exec mv {} ~/pics \; && \
 find ~/pics "$style" ! \( -iname \*jpg -o -iname \*jpeg -o -iname \*jpg_ -o \
