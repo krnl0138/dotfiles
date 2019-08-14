3d78895f171913d4a77204a4c3531cded8f9cda9
kudinov <krnl03182@gmail.com>
2019-08-14 22:47:01 +0300

    regular auto update

--- home/scripts/scripts/collector.sh
+++ home/scripts/scripts/collector.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
-location="$HOME/Downloads"
-comm="find -maxdepth 1 -type f"
-"$comm" "$location" \( -iname \*pdf \) -exec mv {} ~/Downloads/pdfs  \;  && \
-"$comm" "$location" \( -iname \*djvu \) -exec mv {} ~/Downloads/djvus  \; && \
-"$comm" "$location" \( -iname \*docx \) -exec mv {} ~/Downloads/docxs \; && \
-"$comm" "$location" \( -iname \*epub \) -exec mv {} ~/Downloads/epubs \; && \
-"$comm" "$location" \( -iname \*fb2 \) -exec mv {} ~/Downloads/fb2s  \; && \
-"$comm" "$location" \( -iname \*ppt \) -exec mv {} ~/Downloads/ppts  \; && \
-"$comm" "$location" \( -iname \*zip \) -exec mv {} ~/Downloads/zips  \; && \
-"$comm" "$location" \( -iname \*jpg -o -iname \*jpeg -o -iname \*jpg_ -o \
+# location="$HOME/Downloads"
+comm="find -maxdepth 1 -type f $location"
+$comm \( -iname \*pdf \) -exec mv {} ~/Downloads/pdfs  \;  && \
+$comm \( -iname \*djvu \) -exec mv {} ~/Downloads/djvus  \; && \
+$comm \( -iname \*docx \) -exec mv {} ~/Downloads/docxs \; && \
+$comm \( -iname \*epub \) -exec mv {} ~/Downloads/epubs \; && \
+$comm \( -iname \*fb2 \) -exec mv {} ~/Downloads/fb2s  \; && \
+$comm \( -iname \*ppt \) -exec mv {} ~/Downloads/ppts  \; && \
+$comm \( -iname \*zip \) -exec mv {} ~/Downloads/zips  \; && \
+$comm \( -iname \*jpg -o -iname \*jpeg -o -iname \*jpg_ -o \
                                         -iname \*jpe -o -iname \*png -o -iname \*png_ -o \
                                         -iname \*gif \) -exec mv {} ~/pics \; && \
 find ~/pics "$style" ! \( -iname \*jpg -o -iname \*jpeg -o -iname \*jpg_ -o \
