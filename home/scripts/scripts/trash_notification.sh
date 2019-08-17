3418a70d35d3ebae6491126a592bcd22ae425f84
kudinov <krnl03182@gmail.com>
2019-08-17 20:16:01 +0300

    regular auto update

--- home/scripts/scripts/trash_notification.sh
+++ home/scripts/scripts/trash_notification.sh
@@ -2,4 +2,4 @@
 #https://askubuntu.com/questions/298608/notify-send-doesnt-work-from-crontab
 export DISPLAY=:0
 eval "export $(egrep -z DBUS_SESSION_BUS_ADDRESS /proc/$(pgrep -u $LOGNAME sway | sed -n 1p)/environ)";
-/usr/bin/notify-send "Clear bin at .local/share/Trash"
+notify-send "Clear bin at .local/share/Trash"
