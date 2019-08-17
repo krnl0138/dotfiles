ad1014ebb4ba058cc70e0aef14d32d6077daaa46
kudinov <krnl03182@gmail.com>
2019-08-17 18:56:01 +0300

    regular auto update

--- home/scripts/scripts/mypackages.sh
+++ home/scripts/scripts/mypackages.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/bash
-#DATE="$(date +%d-%m-%Y)"
-NAME="/home/gppk/backup/mypackages_$(date +%d-%m-%Y).txt"
+#https://askubuntu.com/questions/298608/notify-send-doesnt-work-from-crontab
+export DISPLAY=:0
+eval "export $(egrep -z DBUS_SESSION_BUS_ADDRESS /proc/$(pgrep -u $LOGNAME sway | sed -n 1p)/environ)";
+FILENAME="$HOME/backup/mypackages_$(date +%d-%m-%Y).txt"
 MESSAGE="CRONJOB: the list of installed pkgs is saved."
-yay -Qe | awk '{print $1}' > "$NAME" && $SWAYSOCK notify-send -t 5000 "$MESSAGE"
+yay -Qe | awk '{print $1}' > "$FILENAME" && notify-send "$MESSAGE"
