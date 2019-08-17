69ab55b5ad1e805fc510a1738fb2bbc8187e3235
kudinov <krnl03182@gmail.com>
2019-08-17 18:46:01 +0300

    regular auto update

--- home/scripts/scripts/gitdots.sh
+++ home/scripts/scripts/gitdots.sh
@@ -4,4 +4,4 @@
 export DISPLAY=:0
 eval "export $(egrep -z DBUS_SESSION_BUS_ADDRESS /proc/$(pgrep -u $LOGNAME sway | sed -n 1p)/environ)";
 cd "$HOME/dotfiles"
-git add * && git commit -am "regular auto update" && git push && notify-send "Dotfiles are synchronized successfully" || notify-send "Synchronization of dots failed "
+git add * && git commit -am "regular auto update" && git push && notify-send "Dotfiles were synchronized successfully" || notify-send "Synchronization of dots failed "
