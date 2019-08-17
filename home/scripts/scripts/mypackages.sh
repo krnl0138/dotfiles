e2a5860020d168e6271635e404310b20aaca4f74
kudinov <krnl03182@gmail.com>
2019-08-17 18:58:01 +0300

    regular auto update

--- home/scripts/scripts/mypackages.sh
+++ home/scripts/scripts/mypackages.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/usr/bin/sh
 #https://askubuntu.com/questions/298608/notify-send-doesnt-work-from-crontab
 export DISPLAY=:0
 eval "export $(egrep -z DBUS_SESSION_BUS_ADDRESS /proc/$(pgrep -u $LOGNAME sway | sed -n 1p)/environ)";
