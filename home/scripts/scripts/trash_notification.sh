34286d7511e742688fcd7a81e63fc2799e259478
kudinov <krnl03182@gmail.com>
2019-08-17 18:26:01 +0300

    regular auto update

--- home/scripts/scripts/trash_notification.sh
+++ home/scripts/scripts/trash_notification.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 export HOME=/home/$notify_user
-export DISPLAY=:0.0
-/usr/bin/notify-send -t 3000 "Clear bin at .local/share/Trash"
+export DISPLAY=:0
+/usr/bin/notify-send "Clear bin at .local/share/Trash"
