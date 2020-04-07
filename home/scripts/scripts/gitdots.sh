35dcc7ce03961f8f7d468023ca92f15c98d86d4e
krnl0138 <krnl03182@gmail.com>
2020-04-07 23:04:01 +0300

    regular auto update

--- home/scripts/scripts/gitdots.sh
+++ home/scripts/scripts/gitdots.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 #https://www.youtube.com/watch?v=6oTzYnQY17Q
 #https://askubuntu.com/questions/298608/notify-send-doesnt-work-from-crontab
-export DISPLAY=:0
-eval "export $(egrep -z DBUS_SESSION_BUS_ADDRESS /proc/$(pgrep -u $LOGNAME sway | sed -n 1p)/environ)";
+# export DISPLAY=:0
+# eval "export $(egrep -z DBUS_SESSION_BUS_ADDRESS /proc/$(pgrep -u $LOGNAME sway | sed -n 1p)/environ)";
 
 SUCCESS_TEXT="CRONJOB: Dotfiles were synced"
 FAIL_TEXT="CRONJOB: Dotfiles were not changed or sync failed"
