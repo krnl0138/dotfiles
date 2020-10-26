43461b7cd5c86bc1aa39f10b4887ea90ce934b07
krnl0138 <krnl03182@gmail.com>
2020-10-26 20:00:02 +0300

    regular auto update

--- home/scripts/scripts/mypackages.sh
+++ home/scripts/scripts/mypackages.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/sh
 #https://askubuntu.com/questions/298608/notify-send-doesnt-work-from-crontab
-export DISPLAY=:0
-eval "export $(egrep -z DBUS_SESSION_BUS_ADDRESS /proc/$(pgrep -u $LOGNAME sway | sed -n 1p)/environ)";
-SUCCESS_TEXT="CRONJOB: the list of installed pkgs was saved at " 
+# export DISPLAY=:0
+# eval "export $(egrep -z DBUS_SESSION_BUS_ADDRESS /proc/$(pgrep -u $LOGNAME sway | sed -n 1p)/environ)";
+SUCCESS_TEXT="CRONJOB: the list of installed pkgs was saved at "
 FAIL_TEXT="CRONJOB: the list of installed pkgs was not saved"
-FILENAME="$HOME/personal/backup/mypackages_$(date +%d-%m-%Y).txt"
+FILENAME="$HOME/personal/dotfiles_backup/mypackages_$(date +%d-%m-%Y).txt"
 yay -Qe | awk '{print $1}' > "$FILENAME" && notify-send "$SUCCESS_TEXT""$FILENAME" || notify-send "$FAIL_TEXT"
+
 
